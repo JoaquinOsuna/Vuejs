1ea9811b8c0028687deaa4af4fe1d20972ac5ed4
Joaquin <joaquin.zatarain@cceo.com.mx>
2021-12-14 13:19:01 -0600

    Se agrego una computed function

--- vue.js
+++ vue.js
@@ -34,3 +34,22 @@ var app = new Vue({
             }
     }
 }) 
+
+new Vue({
+    el: '#vm',
+    data: {
+        primero: 0,
+        segundo: 0,
+        tercero: 0,
+        cuarto: 0,
+        quinto: 0,
+        suma: 0
+    },
+    computed: {
+        total: function(){
+            return this.primero + this.segundo + this.tercero + this.cuarto;
+        }
+    }
+})
+
+
