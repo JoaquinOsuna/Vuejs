7a25c9e36e0724eb2a600ae201e31f51ba39622a
Joaquin <joaquin.zatarain@cceo.com.mx>
2021-12-14 12:25:44 -0600

    Metodos Vuejs

--- vue.js
+++ vue.js
@@ -2,5 +2,10 @@ var app = new Vue({
     el: '.vue',
     data: {
         mensaje: 'Aprende Vue.js Facilmente'
+    },
+    methods: {
+        mostrarMensaje: function(){
+            return 'Aprende Vue.js Facilmente - MM'
+        }
     }
 }) 
