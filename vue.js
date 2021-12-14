192b43c362009e18292e031cdc161d62604560eb
Joaquin <joaquin.zatarain@cceo.com.mx>
2021-12-14 12:59:42 -0600

    Se agregaron metodos

--- vue.js
+++ vue.js
@@ -1,11 +1,36 @@
 var app = new Vue({
     el: '.vue',
     data: {
-        mensaje: 'Aprende Vue.js Facilmente'
+        mensaje: 'Aprende Vue.js Facilmente',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png'
     },
+        beforeCreate: function(){
+            console.log('Llamando beforeCreate');
+        },
+        created: function(){
+            console.log('Llamando create');
+        },
+        beforeMount: function(){
+            console.log('Llamando beforeMount');
+        },
+        mounted: function(){
+            console.log('Llamando mounted');
+        },
+        beforeUpdate: function(){
+            console.log('Llamando beforeUpdate');
+        },
+        updated: function(){
+            console.log('Llamando updated');
+        },
+        beforeDestroy: function(){
+            console.log('Llamando beforeDestroy');
+        },
+        destroyed: function(){
+            console.log('Llamando destroyed');
+        },
     methods: {
-        mostrarMensaje: function(){
-            return 'Aprende Vue.js Facilmente - MM'
-        }
+            destruir : function(){
+                this.$destroy();
+            }
     }
 }) 
